c8da25a91bdcff8bd793b5eaf5ac9a602d1b11b1
Hoang Van Tri <trihoangvan16092003@gmail.com>
2025-05-27 17:25:49 +0700

    NodeJS & Express & MongoDB #20 - Update data 4:28

--- modules/product/product.router.js
+++ modules/product/product.router.js
@@ -4,14 +4,13 @@ const prefix = '/products'; // Test url : http://localhost:3000/products
 // import controller
 const productController = require('./product.controller');
 
-// Goi den function getAllproduct
-// Goi den function getAllproduct
+// Goi den function
 router.get(prefix, productController.getAllproduct);
-router.get(`${prefix}/detail/:id`, productController.getProducrById); 
+router.get(`${prefix}/detail/:id`, productController.getDetailProduct);
+router.get(`/api${prefix}/detail/:id`, productController.getDetailProductByApi);
+router.delete(`${prefix}/detail/:id`, productController.deleteProduct); 
 router.get(`${prefix}/create`, productController.createProduct); 
 router.post(`${prefix}/create`, productController.postCreateProduct); 
-router.post(prefix, productController.createProduct);
-router.put(prefix, productController.updateProduct);
-router.delete(prefix, productController.deleteProduct);
+router.put(`${prefix}/edit/:id`, productController.putEditProduct); 
 
 module.exports = router;
